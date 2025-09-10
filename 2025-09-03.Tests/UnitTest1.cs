8800c6315e1f51b13d627791b50177f5f3046875
Mateo Allmer <ALL230088@spengergasse.at>
2025-09-10 11:22:11 +0200

    Commit

---
+++ 2025-09-03.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace _2025_09_03.Tests;
+
+public class FractionTests
+{
+    [Fact]
+    public void TestAddition()
+    {
+        // Arrange
+        Fraction f1 = Fraction.Parse("1 1/2");
+        Fraction f2 = Fraction.Parse("1/4");
+        string expected = "1 3/4";
+
+        // Act
+        Fraction result = f1 + f2;
+        string actual = result.ToMixedString();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
